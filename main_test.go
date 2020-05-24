dd3e878659409826fbc8f71a414a6348d961d122
Dylan Arbour <dylan.arbour@gmail.com>
2020-05-24 17:40:46 -0400

    Add GETPATH tests

--- main_test.go
+++ main_test.go
@@ -1,8 +1,49 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/mitchellh/go-homedir"
 )
+
+func TestGetPath(t *testing.T) {
+	home, _ := homedir.Dir()
+	dir, _ := ioutil.TempDir("", "git-get")
+	defer os.RemoveAll(dir)
+
+	cases := map[string]struct {
+		pathenv string
+		want    string
+	}{
+		"default": {
+			want: filepath.Join(home, "src"),
+		},
+		"custom": {
+			pathenv: filepath.Join(dir, "src"),
+			want:    filepath.Join(dir, "src"),
+		},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			// Empty GETPATH if set for tests
+			os.Setenv("GETPATH", "")
+			if c.pathenv != "" {
+				os.Setenv("GETPATH", c.pathenv)
+			}
+
+			path, err := getPath()
+			if err != nil {
+				t.Fatalf("unexpected error:\n\t(GOT): %#v\n\t(WNT): nil", err)
+			} else if path != c.want {
+				t.Fatalf("unexpected GETPATH:\n\t(GOT): %#v\n\t(WNT): %#v", path, c.want)
+			}
+		})
+	}
+}
 
 func TestClean(t *testing.T) {
 	cases := map[string]struct {
