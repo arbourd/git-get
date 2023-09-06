9f2c3f741882920210fed3e49bd5a9d39852c457
Dylan Arbour <arbourd@users.noreply.github.com>
2023-09-05 23:49:59 -0400

    Add SSH protocol support
    0.4.0 moved away from the `vcs` pkg which fixed issues with Gitlab
    subgoups but removed ssh and other protocol support.
    
    This commit returns support for all other protocols and schemes. If a
    protocol or scheme are not provided HTTPS will be used as default.

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/url"
 	"os"
 	"path/filepath"
 	"testing"
@@ -44,38 +45,83 @@ func TestGetPath(t *testing.T) {
 	}
 }
 
-func TestClean(t *testing.T) {
+func TestParseURL(t *testing.T) {
 	cases := map[string]struct {
 		remote string
 		want   string
 	}{
 		"git protocol": {
-			remote: "git://github.com/arbourd/git-get",
-			want:   "github.com/arbourd/git-get",
+			remote: "git://github.com/arbourd/git-get.git",
+			want:   "git://github.com/arbourd/git-get.git",
 		},
 		"https protocol": {
-			remote: "https://github.com/arbourd/git-get",
-			want:   "github.com/arbourd/git-get",
+			remote: "https://github.com/arbourd/git-get.git",
+			want:   "https://github.com/arbourd/git-get.git",
 		},
-		".git removal": {
-			remote: "github.com/arbourd/git-get.git",
-			want:   "github.com/arbourd/git-get",
+		"ssh protocol": {
+			remote: "git@github.com:arbourd/git-get.git",
+			want:   "ssh://git@github.com/arbourd/git-get.git",
 		},
-		"filepath": {
-			remote: "github.com///arbourd/git-get",
-			want:   "github.com/arbourd/git-get",
-		},
-		"gitlab subgroups": {
-			remote: "gitlab.com/gitlab-org/dev-subdepartment/ai-experimentation-chrome-plugin",
-			want:   "gitlab.com/gitlab-org/dev-subdepartment/ai-experimentation-chrome-plugin",
+		"no protocol": {
+			remote: "github.com/arbourd/git-get",
+			want:   "https://github.com/arbourd/git-get",
 		},
 	}
 
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
-			remote := clean(c.remote)
-			if remote != c.want {
-				t.Fatalf("unexpected cleaned string:\n\t(GOT): %#v\n\t(WNT): %#v", remote, c.want)
+			url, _ := ParseURL(c.remote)
+			if url.String() != c.want {
+				t.Fatalf("unexpected parsed url string:\n\t(GOT): %#v\n\t(WNT): %#v", url.String(), c.want)
+			}
+		})
+	}
+}
+
+func TestParseDirectory(t *testing.T) {
+	cases := map[string]struct {
+		url  *url.URL
+		want string
+	}{
+		"https protocol": {
+			url: &url.URL{
+				Scheme: "https",
+				Host:   "github.com",
+				Path:   "arbourd/git-get",
+			},
+			want: "github.com/arbourd/git-get",
+		},
+		"ssh protocol": {
+			url: &url.URL{
+				Scheme: "ssh",
+				Host:   "github.com",
+				Path:   "arbourd/git-get",
+			},
+			want: "github.com/arbourd/git-get",
+		},
+		".git removal": {
+			url: &url.URL{
+				Scheme: "https",
+				Host:   "github.com",
+				Path:   "arbourd/git-get.git",
+			},
+			want: "github.com/arbourd/git-get",
+		},
+		"multiple slashes": {
+			url: &url.URL{
+				Scheme: "https",
+				Host:   "github.com",
+				Path:   "arbourd///git-get",
+			},
+			want: "github.com/arbourd/git-get",
+		},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			dir := ParseDirectory(c.url)
+			if dir != filepath.Clean(c.want) {
+				t.Fatalf("unexpected parsed directory string:\n\t(GOT): %#v\n\t(WNT): %#v", dir, filepath.Clean(c.want))
 			}
 		})
 	}
@@ -86,31 +132,56 @@ func TestDownload(t *testing.T) {
 	defer os.RemoveAll(dir)
 
 	cases := map[string]struct {
-		remote string
-		want   string
-		err    bool
+		url  *url.URL
+		want string
+		err  bool
 	}{
-		"github clone": {
-			remote: "github.com/arbourd/git-get",
-			want:   filepath.Join(dir, "github.com/arbourd/git-get"),
+		"clone github": {
+			url: &url.URL{
+				Scheme: "https",
+				Host:   "github.com",
+				Path:   "arbourd/git-get",
+			},
+			want: filepath.Join(dir, "github.com/arbourd/git-get"),
 		},
-		"gitlab clone": {
-			remote: "gitlab.com/gitlab-org/dev-subdepartment/ai-experimentation-chrome-plugin",
-			want:   filepath.Join(dir, "gitlab.com/gitlab-org/dev-subdepartment/ai-experimentation-chrome-plugin"),
+		"clone ssh github": {
+			url: &url.URL{
+				Scheme: "ssh",
+				User:   url.User("git"),
+				Host:   "github.com",
+				Path:   "arbourd/git-get.git",
+			},
+			want: filepath.Join(dir, "github.com/arbourd/git-get"),
 		},
-		"invalid remote": {
-			remote: "https://github.com////arbourd/git-get",
-			err:    true,
+		"clone gitlab subgroups": {
+			url: &url.URL{
+				Scheme: "https",
+				Host:   "gitlab.com",
+				Path:   "gitlab-org/dev-subdepartment/ai-experimentation-chrome-plugin",
+			},
+			want: filepath.Join(dir, "gitlab.com/gitlab-org/dev-subdepartment/ai-experimentation-chrome-plugin"),
 		},
-		"not found remote": {
-			remote: "github.com/arbourd/definitely-doesnt-exist",
-			err:    true,
+		"invalid url": {
+			url: &url.URL{
+				Scheme: "https",
+				Host:   "github.com",
+				Path:   "///arbourd/git-get",
+			},
+			err: true,
+		},
+		"not found": {
+			url: &url.URL{
+				Scheme: "https",
+				Host:   "github.com",
+				Path:   "definitely-doesnt-exist",
+			},
+			err: true,
 		},
 	}
 
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
-			path, err := download(dir, c.remote)
+			path, err := Download(c.url, c.want)
 
 			if err != nil && !c.err {
 				t.Fatalf("unexpected error:\n\t(GOT): %#v\n\t(WNT): nil", err)
